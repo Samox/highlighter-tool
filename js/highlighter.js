90033a7ee916e5dd04b55217150a65e3cd4f1bf6
Sammy Teillet <sammy.teillet@gmail.com>
2024-04-01 13:04:37 +0200

    feature: add iterator

--- js/highlighter.js
+++ js/highlighter.js
@@ -10,8 +10,49 @@ function highlightElements(selector, getParent = false) {
   });
 }
 
-// Example usage: Highlight all paragraphs and elements with the .important-info class
+function removeHighlight() {
+  const highlighted = document.querySelectorAll(".highlighted");
+  highlighted.forEach((el) => {
+    el.classList.remove("highlighted");
+  });
+}
+
+const steps = [
+  {
+    selector: "h2",
+  },
+  {
+    selector: "input[type='submit']",
+    parent: true,
+  },
+];
+
+function* stepGenerator(steps) {
+  for (let i = 0; i < steps.length; i++) {
+    yield steps[i];
+  }
+}
+
+const gen = stepGenerator(steps);
+
+function nextStep() {
+  removeHighlight();
+  const step = gen.next();
+  if (!step.done) {
+    const elements = document.querySelectorAll(step.value.selector);
+    elements.forEach((el) => {
+      el.addEventListener("click", function handler() {
+        el.removeEventListener("click", handler);
+        nextStep();
+      });
+      if (step.value.parent) {
+        el = el.parentNode;
+      }
+      el.classList.add("highlighted");
+    });
+  }
+}
+
 document.addEventListener("DOMContentLoaded", (event) => {
-  highlightElements('input[type="submit"]', true);
-  highlightElements("h2");
+  nextStep();
 });
